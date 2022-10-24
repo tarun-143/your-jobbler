4299870d11fd9928bd21fb4039ac8d0e4274aaff
tarun-strrd <tarunsayyapureddi123@gmail.com>
2022-10-24 08:29:36 +0530

    another-small-change

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(rateLimiter);
 const connectDB=require('./db/connect');
 app.use(express.json());
 // extra packages
 
 // routes
 app.get('/', (req, res) => {
+  console.log('sss')
   res.send('jobs-api');
 });
 app.use('/api/v1/auth',authRouter);
@@ -40,7 +40,7 @@ app.use('/api/v1/jobs',authenticateUser,jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
 
